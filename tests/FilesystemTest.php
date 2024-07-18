db23579985702ee8835627eb93cc4abe0602a8ae
Максим Спирков <spirkov.2001@mail.ru>
2024-07-18 12:50:03 +0400

    Added some tests and optimized existing

--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -12,6 +12,8 @@ class FilesystemTest extends TestCase
 {
     private const FILE_NAME = 'test.txt';
     private const FILE_CONTENT = 'Test file';
+    private const FILE_SIZE = 9;
+    private const FILE_MIME_TYPE = 'text/plain';
 
     private const TEST_DIRECTORY = 'test-dir';
     private const NON_EMPTY_DIRECTORY = 'non-empty';
@@ -28,7 +30,7 @@ class FilesystemTest extends TestCase
         $this->filesystem = new Filesystem(dirname(__DIR__) . '/test-tmp');
     }
 
-    public function testDeletingDirectory()
+    public function testCreatingAndDeletingDirectory()
     {
         $this->filesystem->createDirectory(self::TEST_DIRECTORY);
         $this->assertTrue($this->filesystem->directoryExists(self::TEST_DIRECTORY));
@@ -39,19 +41,7 @@ class FilesystemTest extends TestCase
         $this->assertFalse($this->filesystem->deleteDirectory(self::NOT_EXISTING_DIRECTORY_NAME));
     }
 
-    public function testUploadingFile()
-    {
-        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
-        $this->assertTrue($this->filesystem->fileExists(self::FILE_NAME));
-    }
-
-    public function testGettingFileContent()
-    {
-        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
-        $this->assertEquals(self::FILE_CONTENT, $this->filesystem->getFileContent(self::FILE_NAME));
-    }
-
-    public function testDeletingFile()
+    public function testUploadingAndDeletingFile()
     {
         $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
         $this->assertTrue($this->filesystem->fileExists(self::FILE_NAME));
@@ -60,6 +50,49 @@ class FilesystemTest extends TestCase
         $this->assertFalse($this->filesystem->fileExists(self::FILE_NAME));
 
         $this->assertFalse($this->filesystem->deleteFile(self::NOT_EXISTING_FILE_NAME));
+    }
+
+    public function testGettingFileContent()
+    {
+        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
+        $this->assertEquals(self::FILE_CONTENT, $this->filesystem->getFileContent(self::FILE_NAME));
+
+        $this->assertNull($this->filesystem->getFileContent(self::NOT_EXISTING_FILE_NAME));
+    }
+
+    public function testGettingFileSize()
+    {
+        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
+        $this->assertEquals(self::FILE_SIZE, $this->filesystem->getFileSize(self::FILE_NAME));
+
+        $this->assertNull($this->filesystem->getFileSize(self::NOT_EXISTING_FILE_NAME));
+    }
+
+    public function testGettingFileMimeType()
+    {
+        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
+        $this->assertEquals(self::FILE_MIME_TYPE, $this->filesystem->getFileMimeType(self::FILE_NAME));
+
+        $this->assertNull($this->filesystem->getFileMimeType(self::NOT_EXISTING_FILE_NAME));
+    }
+
+    public function testGettingFileLastModifiedTime()
+    {
+        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
+        $this->assertEquals(time(), $this->filesystem->getFileLastModifiedTime(self::FILE_NAME));
+
+        $this->assertNull($this->filesystem->getFileLastModifiedTime(self::NOT_EXISTING_FILE_NAME));
+    }
+
+    public function testListPathnames()
+    {
+        $this->filesystem->uploadFile(self::FILE_NAME, self::FILE_CONTENT);
+        $this->assertEquals(
+            [$this->filesystem->prepareFullPath(self::FILE_NAME)],
+            $this->filesystem->listPathnames('*')
+        );
+
+        $this->assertEquals([], $this->filesystem->listPathnames(self::NOT_EXISTING_DIRECTORY_NAME));
     }
 
     public function testCopyingFile()
@@ -81,7 +114,7 @@ class FilesystemTest extends TestCase
         $this->assertTrue($this->filesystem->fileExists(self::MOVING_DIRECTORY . '/' . self::FILE_NAME));
     }
 
-    public function testDeletingAllDirectory()
+    public function testDeletingAllDirectories()
     {
         $nonEmptyDirectory = self::TEST_DIRECTORY . '/' . self::NON_EMPTY_DIRECTORY;
 
@@ -93,30 +126,24 @@ class FilesystemTest extends TestCase
 
         $this->filesystem->deleteAllDirectories(self::TEST_DIRECTORY);
 
-        for ($i = 0; $i < 3; $i++) {
-            $this->assertFalse($this->filesystem->directoryExists(self::TEST_DIRECTORY . "/$i"));
-        }
+        $this->assertEquals([], $this->filesystem->listPathnames(self::TEST_DIRECTORY . '/*'));
 
-        $this->assertFalse($this->filesystem->directoryExists($nonEmptyDirectory));
+        $this->assertTrue($this->filesystem->directoryExists(self::TEST_DIRECTORY));
+
+        $this->filesystem->deleteDirectory(self::TEST_DIRECTORY);
     }
 
     public function testRecursiveDeletingEmptyDirectories()
     {
-        $nonEmptyDirectory = self::TEST_DIRECTORY . '/' . self::NON_EMPTY_DIRECTORY;
-
-        $this->filesystem->uploadFile($nonEmptyDirectory . '/' . self::FILE_NAME, self::FILE_CONTENT);
-
         for ($i = 0; $i < 3; $i++) {
             $this->filesystem->createDirectory(self::TEST_DIRECTORY . "/$i/$i");
         }
 
         $this->filesystem->deleteEmptyDirectories(self::TEST_DIRECTORY);
 
-        for ($i = 0; $i < 3; $i++) {
-            $this->assertFalse($this->filesystem->directoryExists(self::TEST_DIRECTORY . "/$i"));
-        }
+        $this->assertEquals([], $this->filesystem->listPathnames(self::TEST_DIRECTORY . '/*'));
 
-        $this->assertTrue($this->filesystem->directoryExists($nonEmptyDirectory));
+        $this->assertTrue($this->filesystem->directoryExists(self::TEST_DIRECTORY));
 
         $this->filesystem->deleteDirectory(self::TEST_DIRECTORY);
     }
@@ -133,11 +160,10 @@ class FilesystemTest extends TestCase
 
         $this->filesystem->deleteEmptyDirectories(self::TEST_DIRECTORY, false);
 
-        for ($i = 0; $i < 3; $i++) {
-            $this->assertFalse($this->filesystem->directoryExists(self::TEST_DIRECTORY . "/$i"));
-        }
-
-        $this->assertTrue($this->filesystem->directoryExists($nonEmptyDirectory));
+        $this->assertEquals(
+            [$this->filesystem->prepareFullPath($nonEmptyDirectory)],
+            $this->filesystem->listPathnames(self::TEST_DIRECTORY . '/*')
+        );
 
         $this->filesystem->deleteDirectory(self::TEST_DIRECTORY);
     }
